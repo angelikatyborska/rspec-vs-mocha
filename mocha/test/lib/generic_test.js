cda2d744338c3a2cb470e3d73ed8d083d4181b8a
Angelika Tyborska <atyborska93@gmail.com>
2016-04-22 18:19:14 +0200

    Update generic js tests

--- mocha/test/lib/generic_test.js
+++ mocha/test/lib/generic_test.js
@@ -1,11 +1,14 @@
 import '../test_helper'
 
 describe('Mocha', () => {
+  // it('will not wait the callback', () => {
+  //   // this test is unpredictable!
+  //   setTimeout(() => {
+  //     expect(false).to.be.true;
+  //   }, 100);
+  // });
+
   it('can wait for a callback', (done) => {
-    setTimeout(() => {
-      expect(true).to.be.true;
-      done();
-      expect(false).to.be.true;
-    }, 1000);
+    setTimeout(() => { console.log('ding!'); done(); }, 100);
   });
 });
